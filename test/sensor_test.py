e121b22eb24a7b0de06c743695ef636b244f536c
ShiningChan <cmjman@163.com>
2014-03-25 14:25:32 +0800

    change dht11 driver

--- test/sensor_test.py
+++ test/sensor_test.py
@@ -3,7 +3,7 @@
 # @Author: ShiningChan
 # @Date:   2014-02-12 16:52:34
 # @Last Modified by:   ShiningChan
-# @Last Modified time: 2014-02-15 15:17:14
+# @Last Modified time: 2014-03-25 14:11:18
 import sys
 sys.path.append('..')
 from sensors.dht11 import DHT11
@@ -11,9 +11,11 @@ from sensors.hc_sr04 import HC_SR04
 from sensors.hc_sr501 import HC_SR501
 
 if __name__ == '__main__':
-    #dht11 = DHT11()   
-    #dht11.getOutput()
+    dht11 = DHT11()   
+    print dht11.getTemperature()
+    print dht11.getHumidity()
+
     #hc_sr04 = HC_SR04()
     #hc_sr04.getOutput()
-    hc_sr501 = HC_SR501()
-    hc_sr501.getOutput()
+    #hc_sr501 = HC_SR501()
+    #hc_sr501.getOutput()
