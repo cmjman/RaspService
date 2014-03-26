fd7d48e325001690b5340e3682e0239c25fd6c58
ShiningChan <cmjman@163.com>
2014-03-26 19:34:06 +0800

    commit

---
+++ scheduler/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: ShiningChan
+# @Date:   2014-03-26 19:33:01
+# @Last Modified by:   ShiningChan
+# @Last Modified time: 2014-03-26 19:33:01
