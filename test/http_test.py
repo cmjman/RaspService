020a2b042c071d554195b2a7cfdb957922ca990c
ShiningChan <cmjman@163.com>
2014-03-30 20:00:46 +0800

    use unit test

--- test/http_test.py
+++ test/http_test.py
@@ -3,66 +3,74 @@
 # @Author: shiningchan
 # @Date:   2014-01-23 16:41:14
 # @Last Modified by:   ShiningChan
-# @Last Modified time: 2014-03-26 19:25:44
+# @Last Modified time: 2014-03-30 18:53:28
 
 import urllib
 import urllib2
-
+import unittest
+import json
 
 HOST_URL = "http://127.0.0.1:8080/service/"
+REMOTE_URL ="http://192.168.1.120:8080/service/"
 
-def post(url, data):
-	req = urllib2.Request(url)
-	data = urllib.urlencode(data)
-	opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
-	response = opener.open(req, data)
-	return response.read()
+class HttpTestCase(unittest.TestCase):
 
-def get(url, data):
-	data = urllib.urlencode(data)
-	full_url = url + '?' + data
-	response = urllib.urlopen(full_url)
-	return response.read()
+	def post(self,url, data):
+		req = urllib2.Request(url)
+		data = urllib.urlencode(data)
+		opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
+		response = opener.open(req, data)
+		return response.read()
 
-def registerTest():
-	posturl = HOST_URL + "register"
-	data = {'nick':'testUser','password':'123456'}
-	print post(posturl, data)
+	def get(url, data):
+		data = urllib.urlencode(data)
+		full_url = url + '?' + data
+		response = urllib.urlopen(full_url)
+		return response.read()
 
-def loginTest():
-	posturl = "http://127.0.0.1:8080/service/login"
-	data = {'user_id':'1','nick':'testUser','password':'123456'}
-	print post(posturl, data)
+	def registerTest():
+		posturl = HOST_URL + "register"
+		data = {'nick':'testUser','password':'123456'}
+		print post(posturl, data)
 
-def addTaskTest(user_id,switch_id):
-	posturl = "http://127.0.0.1:8080/service/addTask"
+	def loginTest():
+		posturl = "http://127.0.0.1:8080/service/login"
+		data = {'user_id':'1','nick':'testUser','password':'123456'}
+		print post(posturl, data)
 
+	def test_addTask(self):
+		user_id =1
+		switch_id = 1
+		posturl =  REMOTE_URL + "addTask"
 
-	if_expression = {"temperature":"20-30","humidity":"12,17"}
-	data = {'user_id':user_id,'switch_id':switch_id,'target_status':'1','if_expression':if_expression}
-	print post(posturl, data)
+		#if_expression = {"temperature":"20-30","humidity":"12,17"}
+		if_expression = {"temperature":">20"}
+		exp_json = json.dumps(if_expression)
+		data = {'user_id':user_id,'switch_id':switch_id,'target_status':'1','if_expression':exp_json}
+		print self.post(posturl, data)
 
-def addSwitchTest(name,level):
-	posturl = "http://127.0.0.1:8080/service/addSwitch"
-	data = {'name':name,'level':level}
-	print post(posturl, data)
+	def addSwitchTest(name,level):
+		posturl = "http://127.0.0.1:8080/service/addSwitch"
+		data = {'name':name,'level':level}
+		print post(posturl, data)
 
-def delSwitchTest():
-	posturl = HOST_URL+ "delSwitch"
-	data = {'switch_id':'2'}
-	print post(posturl, data)
+	def delSwitchTest():
+		posturl = HOST_URL+ "delSwitch"
+		data = {'switch_id':'2'}
+		print post(posturl, data)
 
-def getSwitchTest(page):
-	posturl = "http://127.0.0.1:8080/service/getSwitch"
-	data = {'page':page}
-	print get(posturl, data)
+	def getSwitchTest(page):
+		posturl = "http://127.0.0.1:8080/service/getSwitch"
+		data = {'page':page}
+		print get(posturl, data)
 
-def changeSwitchStatusTest(switch_id,status):
-	posturl = "http://127.0.0.1:8080/service/changeSwitchStatus"
-	data = {'switch_id':switch_id,'status':status}
-	print post(posturl, data)
+	def changeSwitchStatusTest(switch_id,status):
+		posturl = "http://127.0.0.1:8080/service/changeSwitchStatus"
+		data = {'switch_id':switch_id,'status':status}
+		print post(posturl, data)
 
 if __name__ == '__main__':
+	unittest.main()
 	#registerTest()
 	#loginTest()
 	#addTaskTest()
--- test/sensor_test.py
+++ test/sensor_test.py
@@ -3,19 +3,43 @@
 # @Author: ShiningChan
 # @Date:   2014-02-12 16:52:34
 # @Last Modified by:   ShiningChan
-# @Last Modified time: 2014-03-25 14:11:18
+# @Last Modified time: 2014-03-30 19:59:57
 import sys
 sys.path.append('..')
-from sensors.dht11 import DHT11
-from sensors.hc_sr04 import HC_SR04
-from sensors.hc_sr501 import HC_SR501
+#from sensors.dht11 import DHT11
+#from sensors.hc_sr04 import HC_SR04
+#from sensors.hc_sr501 import HC_SR501
+
+import time
+import unittest
+
+class SensorTestCase(unittest.TestCase):
+
+	def _test_dht11(self):
+		dht11 = DHT11(4)   
+		print dht11.getTemperature()
+		print dht11.getHumidity()
+
+	def test_time(self):
+		
+		a=time.time()
+		i=1
+		c=time.time()
+		d=c-a
+		print d
+
+		a=time.time()
+		for i in range(n):
+			j+=1
+		c=time.time()
+		d=c-a
+		print d
 
 if __name__ == '__main__':
-    dht11 = DHT11(4)   
-    print dht11.getTemperature()
-    print dht11.getHumidity()
 
-    #hc_sr04 = HC_SR04()
-    #hc_sr04.getOutput()
-    #hc_sr501 = HC_SR501()
-    #hc_sr501.getOutput()
+	unittest.main()
+  
+	#hc_sr04 = HC_SR04()
+	#hc_sr04.getOutput()
+	#hc_sr501 = HC_SR501()
+	#hc_sr501.getOutput()
